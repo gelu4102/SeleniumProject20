938a0a0f008499ce94f863f90a159abea9df0d4c
gelu4102 <sabukuk@gmail.com>
2020-09-27 09:34:01 -0500

    VerifyName

--- src/test/java/com/cybertek/Utilities/SmartBearUtilities.java
+++ src/test/java/com/cybertek/Utilities/SmartBearUtilities.java
@@ -3,6 +3,9 @@ package com.cybertek.Utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class SmartBearUtilities {
     //Mini-Task:
@@ -24,4 +27,26 @@ public class SmartBearUtilities {
             WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
             loginButton.click();
         }
+//PRACTICE #4: Method: verifyOrder
+// •Create a method named verifyOrderin SmartBearUtils class.
+// •Method takes WebDriver object and String(name).
+// •Method should verify if given name exists in orders.
+// •This method should simply accepts a name(String), and assert whether given name is in the list or not.
+// •Create a new TestNG test to test if the method is working as expected
+
+    public static void verifyOrder(WebDriver driver,String name){
+
+            List<WebElement> namesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[2]"));
+
+
+         for (WebElement each : namesList) {
+             if (each.getText().equals(name)){
+                 Assert.assertTrue(true);
+                 return;
+             }
+         }
+         Assert.fail("Name does not exist in the list!");
+     }
+
+
     }
